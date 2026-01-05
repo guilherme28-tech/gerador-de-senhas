150e2809eba68bf13d48b012cf4c73a2556dfd55
guilherme28-tech <guif4080@gmail.com>
2026-01-05 02:28:56 -0300

    Update gerador.py
    alterei a parte de escolher os caracteres da senha, simplifiquei pra ficar com uma lista só. Modifiquei tambem os 2 primeiros inputs, deixei os 2 em um só padrão (tirei os comentarios sem querer, perdão :))

--- gerador.py
+++ gerador.py
@@ -1,61 +1,50 @@
 import random
 
-print('=================================\n||      Gerador de Senhas      ||\n=================================')
+print('='*30)
+print('{:^30}'.format('GERADOR DE SENHAS'))
+print('='*30)
 
-num_senhas = int(input("Número de senhas: ") or 1)
+num_senhas = 0
+while num_senhas < 1:
+    num_senhas = int(input("Número de senhas: "))
+    if num_senhas < 1:
+       print("Ocorreu um erro na geração de senhas!")
 tamanho_senha = 0
 
-# tamanho mínimo da senha deve ser 8 dígitos
 while tamanho_senha < 8:
-    tamanho_senha = int(input("Total de dígitos na senha: ") or "8")
+    tamanho_senha = int(input("Quantidade de dígitos na senha: "))
     if tamanho_senha < 8:
-        print('A senha deve conter pelo menos 8 dígitos.')
-        
-print('=================================\n')
+        print("A senha deve conter pelo menos 8 dígitos.")
 
-# possíveis caracteres
-especiais = [ '.', '+', '-', '@', '#', '*', '!', '?', '~']
-numeros = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-letras_min = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-letras_mai = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+caracteres = ['.', '+', '-', '@', '#', '*', '!','?', '~'
+'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+'v', 'w', 'x', 'y', 'z' 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+'Q', 'R', 'S', 'T', 'U',
+'V', 'W', 'X', 'Y', 'Z']
 
-# função para gerar senhas
+
+
 def geraSenha(n_senha, t_senha):
     senhas = []
     i = 1
-    
+
     while i <= n_senha:
         senha = []
-        
+
         while len(senha) < t_senha:
-            senha.append(random.choice(especiais))
+            senha.append(random.choice(caracteres))
             if len(senha) == t_senha:
                 break
-            senha.append(random.choice(numeros))
-            if len(senha) == t_senha:
-                break
-            senha.append(random.choice(letras_min))
-            if len(senha) == t_senha:
-                break
-            senha.append(random.choice(letras_mai))
-            
-        senhas.append(senha)            
+        senhas.append(senha)
         i = i + 1
-        
+
     return senhas
 
-    
+
 senhas_geradas = geraSenha(num_senhas, tamanho_senha)
 
-# imprimindo as senhas
 j = 1
 for senha in senhas_geradas:
     password = ''.join(senha)
     print(f'Senha {j}: {password} \n')
     j = j + 1
-    
-print('=================================\n')
-    
-    
-   
-    
